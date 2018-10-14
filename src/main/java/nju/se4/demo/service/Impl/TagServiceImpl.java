b20e650fc1cceef1eeb475d98b8361095ebb3723
2529716798 <2529716798@qq.com>
2018-10-14 18:08:01 +0800

    犹豫不决

---
+++ src/main/java/nju/se4/demo/service/Impl/TagServiceImpl.java
@@ -0,0 +1,20 @@
+package nju.se4.demo.service.Impl;
+
+import nju.se4.demo.domain.Tag;
+import nju.se4.demo.service.TagService;
+import nju.se4.demo.util.Response;
+
+import java.util.List;
+
+public class TagServiceImpl implements TagService {
+    @Override
+    public Response<Tag> addTag(String type, String name) {
+
+        return null;
+    }
+
+    @Override
+    public Response<List<Tag>> getTagListByUserId(String userId) {
+        return null;
+    }
+}
---
+++ src/main/java/nju/se4/demo/service/TagService.java
@@ -0,0 +1,23 @@
+package nju.se4.demo.service;
+
+import nju.se4.demo.domain.Tag;
+import nju.se4.demo.util.Response;
+
+import java.util.List;
+
+public interface TagService {
+    /**
+     * 增加标签
+     * @param type
+     * @param name
+     * @return
+     */
+    Response<Tag> addTag(String type, String name);
+
+    /**
+     * 查找userId的用户的所有标签
+     * @param userId
+     * @return
+     */
+    Response<List<Tag>> getTagListByUserId(String userId);
+}
