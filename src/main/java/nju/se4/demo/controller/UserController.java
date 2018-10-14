aebe2f8f68c4d44a4e009074915f32a9e597ad4c
2529716798 <2529716798@qq.com>
2018-10-14 18:09:02 +0800

    add @ResponseBody

--- src/main/java/nju/se4/demo/controller/UserController.java
+++ src/main/java/nju/se4/demo/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(value = "/api/vX/user/")
@@ -16,17 +17,20 @@ public class UserController {
     private UserService userService;
 
     @RequestMapping(value = "/{userId}", method = RequestMethod.GET)
+    @ResponseBody
     public Response<UserVO> getUserById(@PathVariable("userId") int id) {
         return userService.getUserById(id);
     }
 
     @RequestMapping(value = "/{userId}", method = RequestMethod.POST)
+    @ResponseBody
     public Response<UserVO> updateUserInfo(@PathVariable("userId") int id,
                                            String nickname, String avatar, String bio) {
         return userService.updateUserInfo(id, nickname, avatar, bio);
     }
 
     @RequestMapping(value = "/{userId}/password", method = RequestMethod.POST)
+    @ResponseBody
     public Response<UserVO> updatePassword(@PathVariable("userId") int id,
                                            String password) {
         return userService.updatePassword(id, password);
