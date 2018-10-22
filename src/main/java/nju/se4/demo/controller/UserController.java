bcfbe89db85a812673382a1b844dd0f024ec51b4
2529716798 <2529716798@qq.com>
2018-10-22 17:52:40 +0800

    Update src/main/java/nju/se4/demo/controller/UserController.java

--- src/main/java/nju/se4/demo/controller/UserController.java
+++ src/main/java/nju/se4/demo/controller/UserController.java
@@ -4,6 +4,7 @@ import nju.se4.demo.service.UserService;
 import nju.se4.demo.util.Response;
 import nju.se4.demo.vo.UserVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,20 +19,20 @@ public class UserController {
 
     @RequestMapping(value = "{userId}", method = RequestMethod.GET)
     @ResponseBody
-    public Response<UserVO> getUserById(@PathVariable("userId") int id) {
+    public Response<UserVO> getUserById(@AuthenticationPrincipal @PathVariable("userId") int id) {
         return userService.getUserById(id);
     }
 
     @RequestMapping(value = "{userId}", method = RequestMethod.POST)
     @ResponseBody
-    public Response<UserVO> updateUserInfo(@PathVariable("userId") int id,
+    public Response<UserVO> updateUserInfo(@AuthenticationPrincipal @PathVariable("userId") int id,
                                            String nickname, String avatar, String bio) {
         return userService.updateUserInfo(id, nickname, avatar, bio);
     }
 
     @RequestMapping(value = "{userId}/password", method = RequestMethod.POST)
     @ResponseBody
-    public Response<UserVO> updatePassword(@PathVariable("userId") int id,
+    public Response<UserVO> updatePassword(@AuthenticationPrincipal @PathVariable("userId") int id,
                                            String password) {
         return userService.updatePassword(id, password);
     }
